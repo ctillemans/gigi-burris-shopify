7bcb0388c41aeffcd99a18fa7dcdd6919a208d97
Cassidy Tillemans <cassidybtillemans@gmail.com>
2021-01-24 13:31:19 -0800

    all submenu links pointing to correct image paths

--- assets/menuNav.js
+++ assets/menuNav.js
@@ -2,3 +2,17 @@ $('.site-image-nav__link').hover(function () {
   var s = $(this).data('imgsrc');
   $('.site-image-nav__menu-image').attr('src', s);
 });
+
+// $('.site-image-nav__listItem--has-dropdown').hover().addClass('open');
+
+$(document).ready(function () {
+  $('.site-image-nav__listItem--has-dropdown').hover(
+    function () {
+      //When trigger is hovered...
+      $(this).children('.dropdown-menu').addClass('open');
+    },
+    function () {
+      $(this).children('.dropdown-menu').removeClass('open');
+    }
+  );
+});

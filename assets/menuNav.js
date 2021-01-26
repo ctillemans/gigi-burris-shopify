b28a4238414d59c76c5e16cc12b109ad6a2446b2
Cassidy Tillemans <cassidybtillemans@gmail.com>
2021-01-26 01:54:16 -0800

    adds media queries, vertical dropdown, image link changes on hover

--- assets/menuNav.js
+++ assets/menuNav.js
@@ -1,6 +1,8 @@
 $('.site-image-nav__link').hover(function () {
   var s = $(this).data('imgsrc');
+  var href = $(this).attr('href');
   $('.site-image-nav__menu-image').attr('src', s);
+  $('.site-image-nav__image-link').attr('href', href);
 });
 
 // $('.site-image-nav__listItem--has-dropdown').hover().addClass('open');
@@ -15,4 +17,15 @@ $(document).ready(function () {
       $(this).children('.dropdown-menu').removeClass('open');
     }
   );
+});
+
+$('.submenu-toggle-button').click(function (e) {
+  e.preventDefault();
+  //When trigger is hovered...
+  $(this).parent().siblings('.site-image-nav__subdropdown').toggleClass('open');
+  if ($(this).hasClass('open')) {
+    $(this).removeClass('open');
+  } else {
+    $(this).addClass('open');
+  }
 });
